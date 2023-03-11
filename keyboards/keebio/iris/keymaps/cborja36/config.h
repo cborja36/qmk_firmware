18329962c6a79c63ad01bd11d8e0d7d261b37e94
cborja36 <cborja36@gmail.com>
2023-03-11 20:11:21 +0100

    simplify layout

--- keyboards/keebio/iris/keymaps/cborja36/config.h
+++ keyboards/keebio/iris/keymaps/cborja36/config.h
@@ -1,3 +1,3 @@
-#define IGNORE_MOD_TAP_INTERRUPT
-#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-
+#define HOLD_ON_OTHER_KEY_PRESS
+#define TAPPING_FORCE_HOLD
+#define USB_SUSPEND_WAKEUP_DELAY 200  // in order to prevent keybaoard not waking up
