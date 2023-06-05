7e9300d93a77e573081500344f5757e92d9980ef
cborja36 <cborja36@gmail.com>
2023-06-05 11:55:18 +0200

    estable

--- keyboards/keebio/iris/keymaps/cborja36/config.h
+++ keyboards/keebio/iris/keymaps/cborja36/config.h
@@ -1,3 +1,13 @@
-#define HOLD_ON_OTHER_KEY_PRESS
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+// #define QUICK_TAP_TERM 0
 #define TAPPING_FORCE_HOLD
+
 #define USB_SUSPEND_WAKEUP_DELAY 200  // in order to prevent keybaoard not waking up
--- keyboards/keebio/iris/keymaps/cborja36/keymap.c
+++ keyboards/keebio/iris/keymaps/cborja36/keymap.c
@@ -51,6 +51,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+		case LT(1,KC_BSPC):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+		case LT(2,KC_TAB):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+		case LT(3,KC_ENT):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -58,16 +75,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LCTL(KC_UP), RGB_TOG, KC_BRID, KC_BRIU, KC_MPLY, KC_MPRV,                                KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, KC_LBRC, KC_RBRC,
 
 		KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_QUOT,
-		KC_LCTL, KC_A,    KC_S,     KC_D,    KC_F,    KC_G,                                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,    LALT(KC_K),
+   		KC_LCTL, LCTL_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,                 KC_H, LGUI_T(KC_J), LALT_T(KC_K), LSFT_T(KC_L), LCTL_T(KC_SCLN), LALT(KC_K),
 		KC_LSFT,   KC_Z,  KC_X,    KC_C,     KC_V,     KC_B, KC_CAPS,                  LT(3,KC_ENT), KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-							        	KC_LGUI, LT(1,KC_BSPC), KC_SPC,           LT(3,KC_ENT), LT(2,KC_TAB), KC_LALT
+							        	KC_NO, LT(1,KC_BSPC), KC_SPC,           LT(3,KC_ENT), LT(2,KC_TAB), KC_NO
 	),
 
 
 	[1] = LAYOUT(
 		KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                                KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
 
-		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                              KC_AGIN, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_NO,
+		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                              KC_NO, KC_NO, KC_NO, KC_COPY, KC_PSTE, KC_NO,
 		KC_LCTL, KC_HYPR, KC_LSFT, KC_LALT, KC_LGUI, KC_NO,                                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
 		KC_NO, KC_NO, KC_NO, KC_LPRN, KC_RPRN, KC_NO,  KC_NO,                        KC_ENT, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO,
 		                            KC_NO, KC_NO, KC_NO,                    KC_ENT, KC_TAB, KC_BSPC
@@ -77,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[2] = LAYOUT(
 		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, QK_BOOT,
 
-		KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                                              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO, KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                                              KC_NO, LGUI(KC_C), LGUI(KC_B), KC_NO, KC_NO, KC_NO,
 		KC_NO, KC_P, KC_4, KC_5, KC_6, KC_EQL,                                               KC_NO, KC_LGUI, KC_LALT, KC_LSFT, KC_HYPR, KC_LCTL,
 		KC_NO, KC_PLUS, KC_1, KC_2, KC_3, KC_BSLS, KC_NO,                                 KC_NO, KC_NO, KC_NO, KC_COMM, KC_DOT, LALT(LSFT(KC_SLSH)), KC_NO,
 		                          KC_MINS, KC_0, KC_SPC,                                 KC_NO, KC_NO, KC_NO
